c2aa3995ef523099a466b707196caa25c58dcf74
Caio Moisés <114321520+caiomoises@users.noreply.github.com>
2023-02-10 19:58:58 -0300

    Update Q2.c

--- struct/lista II/Q2.c
+++ struct/lista II/Q2.c
@@ -7,69 +7,79 @@ typedef struct pessoa{
     int CPF;
 }Pessoa;
 
-void preenche(Pessoa * pessoa, int n) {
+void preenche(Pessoa ** pessoa, int n) {
     for(int i = 0; i < n; i++) {
         printf("\n\tDados pessoais: N - %d\n", i+1);
 
         printf("Nome: ");
-        scanf(" %[^\n]s", pessoa[i].nome);
+        scanf(" %[^\n]s", pessoa[i]->nome);
 
         printf("Idade: ");
-        scanf("%d", &pessoa[i].idade);
+        scanf("%d", &pessoa[i]->idade);
         
         printf("Documento: ");
-        scanf("%d", &pessoa[i].CPF);
+        scanf("%d", &pessoa[i]->CPF);
     }
 }
 
-void imprime(Pessoa * pessoa, int n) {
+void imprime(Pessoa ** pessoa, int n) {
     printf("\n");
     for(int i = 0; i < n; i++) {
-        printf("\nnome da pessoa: %s\n", pessoa[i].nome);
+        printf("\nnome da pessoa: %s\n", pessoa[i]->nome);
 
-        printf("numero do documento da pessoa: %d\n", pessoa[i].CPF);
+        printf("numero do documento da pessoa: %d\n", pessoa[i]->CPF);
 
-        printf("idade da pessoa: %d\n", pessoa[i].idade);
+        printf("idade da pessoa: %d\n", pessoa[i]->idade);
     }
 
 }
 
-void altera_idade(Pessoa * pessoa, int n) {
+void altera_idade(Pessoa ** pessoa, int n) {
     printf("\n");
     for(int i = 0; i < n; i++) {
         printf("Infome a nova idade: ");
-        scanf("%d", &pessoa[i].idade);
+        scanf("%d", &pessoa[i]->idade);
     }
 }
 
-void maior_menor(Pessoa * pessoa, int n) {
+void maior_menor(Pessoa ** pessoa, int n) {
     int i, maior = 0, menor = 0;
     for (i = 0; i < n; i++) {
-        if (pessoa[i].idade > pessoa[maior].idade) {
+        if (pessoa[i]->idade > pessoa[maior]->idade) {
             maior = i;
         }
-        if (pessoa[i].idade < pessoa[menor].idade) {
+        if (pessoa[i]->idade < pessoa[menor]->idade) {
             menor = i;
         }
     }
-    printf("\nMaior idade: %d", pessoa[maior].idade);
-    printf("\nMenor idade: %d", pessoa[menor].idade);
+    printf("\nMaior idade: %d", pessoa[maior]->idade);
+    printf("\nMenor idade: %d", pessoa[menor]->idade);
 } 
 
 int main(void){
     int n;
     printf("Quantidade de pessoas: ");
     scanf("%d", &n);
-    Pessoa* pessoa = (Pessoa*) malloc(n * sizeof(Pessoa));
+    Pessoa ** pessoa = (Pessoa**) malloc(n * sizeof(Pessoa*));
 
     if(pessoa == NULL) {
         exit(1);
+    }
+
+    for(int i = 0; i< n; i++){
+        pessoa[i] = (Pessoa*) malloc(n * sizeof(Pessoa));
     }
 
     preenche(pessoa, n);
     imprime(pessoa, n);
     altera_idade(pessoa, n);
     maior_menor(pessoa, n);
+
+    for(int i = 0; i< n; i++){
+
+        free(pessoa[i]);
+        
+    }
 
     free(pessoa);
 
