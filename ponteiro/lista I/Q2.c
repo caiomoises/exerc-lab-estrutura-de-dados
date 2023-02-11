b0d2f11bb1351408e671430dc4b799b5e69f16e2
Caio Moisés <114321520+caiomoises@users.noreply.github.com>
2023-02-11 18:08:07 -0300

    Update Q2.c

--- ponteiro/lista I/Q2.c
+++ ponteiro/lista I/Q2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(void){
+    int x, *p;
+    x = 100;
+    p = x;
+    //*p = &p;
+    printf("Valor de p: %d \nValor de *p: %p", p, p);
+    return 0;
+}
+
+/*a) Esta mensagem é de erro ou advertência? nada sera exibido;
+b) Por que o compilador emite tal mensagem? por causa do codigo de formatação, %p seria para *p e %d seria para p;
+c) Compile e execute o programa. A execução foi bem sucedida? Sim, porém nada foi exibido;
+d) Modifique o trecho de código acima, de modo que nenhuma mensagem seja emitida 
+pelo compilador.
+e) Compile e execute novamente o programa. A execução foi bem sucedida? Sim*/
