b1976a7014b1a9b839dc1d47c4c5a6b4917447a3
Caio Moisés <114321520+caiomoises@users.noreply.github.com>
2023-02-11 18:13:11 -0300

    Update Q3.c

--- ponteiro/lista I/Q3.c
+++ ponteiro/lista I/Q3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(void) {
+    int a, b, c, d, *p1, *p2 = &a, *p3 = &c;
+    
+    p1 = p2;
+    *p2 = 10;
+    b = 20;
+    int **pp;
+    pp = &p1;
+    *p3 = **pp;
+    int *p4 = &d;
+    *p4 = b + (*p1)++;
+
+    printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+    
+    return 0;
+}
