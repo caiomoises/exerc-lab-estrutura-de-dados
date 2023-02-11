8a5c22463806732159dd4f9ce7acd51a375b0f94
Caio Moisés <114321520+caiomoises@users.noreply.github.com>
2023-02-11 17:14:59 -0300

    Update Q1.c

--- ponteiro/lista I/Q1.c
+++ ponteiro/lista I/Q1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void){
+    int x, y, *p;
+    y = 0;
+    p = &y;
+    x = *p;
+    x = 4;
+    (*p)++;
+    --x;
+    (*p) += x;
+    printf("Valor de X: %i \nValor de Y: %i \nValor de *p: %p", x, y, p);
+}
