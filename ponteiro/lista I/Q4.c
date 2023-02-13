24e4c16a21fb881ac3d98e4f4669f46a1c8ce5f5
Caio Moisés <114321520+caiomoises@users.noreply.github.com>
2023-02-13 08:26:42 -0300

    Update Q4.c

--- ponteiro/lista I/Q4.c
+++ ponteiro/lista I/Q4.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <math.h>
 
+void calcula_hexagono(float l, float * area, float * perimetro){
+    *area = (3*pow(l,2)*sqrt(3))/2;
+    *perimetro = 6*l;
+}
+
+int main(void){
+    float l, area, perimetro;
+    printf("Insira o valor de L: ");
+    scanf("%f", &l);
+
+    calcula_hexagono(l, &area, &perimetro);
+
+    printf("Area = %.2f\n", area);
+    printf("Perimetro = %.2f", perimetro);
+
+    return 0;
+}
